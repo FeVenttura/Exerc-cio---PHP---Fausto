b1b58a79b10c84547bee52919748ac1e0f282056
FeVenttura <fe.vntr@gmail.com>
2024-09-11 21:54:33 -0300

    mini alterações

--- includes/menu.php
+++ includes/menu.php
@@ -2,5 +2,5 @@
                     <li><a href="index.php">Inicial</a></li>
                     <li><a href="sobre.php">Sobre</a></li>
                     <li><a href="contato.php">Contato</a></li>
-                    <li><a href="servicos.php">Servicos</a></li>
+                    <li><a href="servicos.php">Serviços</a></li>
                 </ul>
